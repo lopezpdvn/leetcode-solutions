145f8de89eff4de005f56fd746b9246fec12f69d
Pedro Ivan Lopez <m@pedroivanlopez.com>
2019-11-11 23:07:37 -0600

    more similar to explanation

--- solutions/3/03.js
+++ solutions/3/03.js
@@ -7,21 +7,20 @@
 const lengthOfLongestSubstring = s => {
   const n = s.length, map = new Map();
   let ans = 0;
-  // try to extend the range [i, j]
   for(let j = 0, i = 0; j < n; j++) {
-    const charAtJ= s.charAt(j);
-    if(map.has(s.charAt(j)))
-      i = Math.max(map.get(charAtJ), i);
+    const charAtJ = s.charAt(j);
+    if(map.has(charAtJ))
+      i = Math.max(map.get(charAtJ) + 1, i);
     ans = Math.max(ans, j - i + 1);
-    map.set(charAtJ, j + 1);
+    map.set(charAtJ, j);
   }
   return ans;
 };
 
 const tests = [
-  ["abcabcbb", 3],
-  ["bbbbb", 1],
-  ["pwwkew", 3],
+  ['abcabcbb', 3],
+  ['bbbbb', 1],
+  ['pwwkew', 3],
 ];
 
 tests.forEach(test => {
