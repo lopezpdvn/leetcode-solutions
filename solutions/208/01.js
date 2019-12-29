5fa5889bf56b51908d3b1b9795ec2fef86808749
Pedro Ivan Lopez <m@pedroivanlopez.com>
2019-12-28 22:19:34 -0600

    dummy implementation 1 with classes

--- solutions/208/01.js
+++ solutions/208/01.js
@@ -1,43 +1,45 @@
 'use strict'; const log = console.log; (async ()=>{
 
-/**
- * Initialize your data structure here.
- */
-var Trie = function() {
-  this.words = new Set();
-};
+class Trie {
+  /**
+   * Initialize your data structure here.
+   */
+  constructor() {
+    this.words = new Set();
+  }
+
+  /**
+   * Inserts a word into the trie.
+   * @param {string} word
+   * @return {void}
+   */
+  insert(word) {
+    this.words.add(word);
+  };
+
+  /**
+   * Returns if the word is in the trie.
+   * @param {string} word
+   * @return {boolean}
+   */
+  search(word) {
+    return this.words.has(word);
+  };
+
+  /**
+   * Returns if there is any word in the trie that starts with the given prefix.
+   * @param {string} prefix
+   * @return {boolean}
+   */
+  startsWith(prefix) {
+    for(const word of this.words)
+      if(word.startsWith(prefix))
+        return true;
+    return false;
+  };
+}
 
 /**
- * Inserts a word into the trie. 
- * @param {string} word
- * @return {void}
- */
-Trie.prototype.insert = function(word) {
-  this.words.add(word);
-};
-
-/**
- * Returns if the word is in the trie. 
- * @param {string} word
- * @return {boolean}
- */
-Trie.prototype.search = function(word) {
-  return this.words.has(word);
-};
-
-/**
- * Returns if there is any word in the trie that starts with the given prefix. 
- * @param {string} prefix
- * @return {boolean}
- */
-Trie.prototype.startsWith = function(prefix) {
-  for(const word of this.words)
-    if(word.startsWith(prefix))
-      return true;
-  return false;
-};
-
-/** 
  * Your Trie object will be instantiated and called as such:
  * var obj = new Trie()
  * obj.insert(word)
