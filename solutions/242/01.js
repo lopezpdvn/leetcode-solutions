6195d4aae06307599919b2ddddcaae74d9253289
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-01-04 18:38:02 -0600

    label as leetcode

--- solutions/242/01.js
+++ solutions/242/01.js
@@ -8,17 +8,10 @@ const isAnagram = (s, t) => {
   if(s.length !== t.length)
     return false;
 
-  const indexOffset = 'a'.charCodeAt();
-  const counter = new Array(26).fill(Number());
+  return new Set(
+    [s, t].map(x =>
+      [...x].sort((a, b) => a.localeCompare(b))
+            .join('')))
+    .size === 1;
 
-  for(let i = 0; i < s.length; i++) {
-    counter[s.charCodeAt(i) - indexOffset]++;
-    counter[t.charCodeAt(i) - indexOffset]--;
-  }
-
-  for(const count of counter)
-    if(count)
-      return false;
-
-  return true;
 };
