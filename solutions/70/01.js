15d7df4cc247bb9841712b525dcb89ff1e4698c1
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-03-07 15:02:26 -0600

    improve solution

--- solutions/70/01.js
+++ solutions/70/01.js
@@ -1,19 +1,13 @@
 'use strict'; const log = console.log; (async ()=>{
 
-const climbStairs = n =>
-  climbStairs_(0, n, new Array(n + 1)
-                       .fill(Number()));
+const climbStairs = n => g(n, new Array(n+1));
 
-const climbStairs_ = function f(i, n, memo) {
-  if(i > n)
-    return 0;
-  if(i === n)
-    return 1;
-  if(memo[i])
-    return memo[i];
-
-  memo[i] = f(i+1, n, memo) + f(i+2, n, memo);
-  return memo[i];
-}
+const g = function f(n, a) {
+  if(n < 0) return 0;
+  if(!n)    return 1;
+  if(!a[n])
+    a[n] = f(n-1, a) + f(n-2, a);
+  return a[n];
+};
 
 })();
