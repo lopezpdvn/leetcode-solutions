2d39d997ff03af2a7eb5d59d52bafe99c3ab1ffd
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-03-13 23:31:14 -0600

    no extra wrapper class, just add counter to tuple

--- solutions/23/03.py
+++ solutions/23/03.py
@@ -6,34 +6,32 @@ from queue import PriorityQueue
 #         self.val = x
 #         self.next = None
 
-class Node:
-  def __init__(self, node):
-    self.node = node
-  def __lt__(self, other):
-    return self.node.val < other.node.val
-
 class Solution:
   def mergeKLists(self,
           lists: List[ListNode]) -> ListNode:
     # Dummy init node that points to the head of
     # result final singly-linked list
     headAns = ListNode(None);
+    i = 0
 
     q = PriorityQueue()
     for l in lists:
       if l:
-        q.put(Node(l))
+        q.put((l.val, i, l))
+        i += 1
 
     node = headAns
     while not q.empty():
       # highest priority node of k nodes
-      nodeMinK = q.get().node
+      valMinK, iMinK, nodeMinK = q.get()
 
       # Append to final list
-      node.next = ListNode(nodeMinK.val)
+      node.next = ListNode(valMinK)
       node = node.next
 
       if nodeMinK.next:
-        q.put(Node(nodeMinK.next))
+        q.put(
+          (nodeMinK.next.val, i, nodeMinK.next))
+        i += 1
 
     return headAns.next
