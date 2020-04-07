4d7d6d7a873637b9613c3deddbe6cf5e95f987b3
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-04-07 16:00:41 -0500

    add solution

--- solutions/986/01.js
+++ solutions/986/01.js
@@ -1,12 +1,21 @@
 'use strict'; const log = console.log; (async ()=>{
 
-/**
- * @param {number[][]} A
- * @param {number[][]} B
- * @return {number[][]}
- */
-var intervalIntersection = function(A, B) {
-    
+const intervalIntersection = (A, B) => {
+  const ans = [];
+  let a = b = 0;
+
+  while(a < A.length && b < B.length) {
+    const intrsctnL = Math.max(A[a][0], B[b][0]);
+    const intrsctnH = Math.min(A[a][1], B[b][1]);
+
+    if(intrsctnL <= intrsctnH)
+      ans.push([intrsctnL, intrsctnH]);
+
+    if(A[a][1] < B[b][1]) a++;
+    else                  b++;
+  }
+
+  return ans;
 };
 
 })();
