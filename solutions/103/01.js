81ac5671a1df73f271ce09dca87ba3f134e890ca
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-04-08 21:29:13 -0500

    leetcode's template

---
+++ solutions/103/01.js
@@ -0,0 +1,18 @@
+'use strict'; const log = console.log; (async ()=>{
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function(root) {
+    
+};
+
+})();
