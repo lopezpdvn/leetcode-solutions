6e40118a99da8af203b13bd53e65c586d699fb83
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-04-08 21:50:24 -0500

    add solution

--- solutions/103/01.js
+++ solutions/103/01.js
@@ -1,18 +1,27 @@
 'use strict'; const log = console.log; (async ()=>{
 
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number[][]}
- */
-var zigzagLevelOrder = function(root) {
-    
+const zigzagLevelOrder = root => {
+  if(!root) return [];
+  const ans = [], nodeQ = [root, null];
+  let isOrdrLeft = true, lvlL = [], node;
+
+  while(nodeQ.length) {
+    node = nodeQ.shift();
+    if(node) {
+      if(isOrdrLeft) lvlL.push   (node.val);
+      else           lvlL.unshift(node.val);
+
+      if(node.left)  nodeQ.push(node.left );
+      if(node.right) nodeQ.push(node.right);
+    }
+    else {
+      ans.push(lvlL);
+      lvlL = [];
+      if(nodeQ.length) nodeQ.push(null);
+      isOrdrLeft = !isOrdrLeft;
+    }
+  }
+  return ans;
 };
 
 })();
