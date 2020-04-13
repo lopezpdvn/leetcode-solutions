30c427bea59cfff51c3cc328e73cff933a215284
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-04-13 05:03:30 -0500

    add solution

--- solutions/62/01.js
+++ solutions/62/01.js
@@ -3,6 +3,14 @@
  * @param {number} n
  * @return {number}
  */
-var uniquePaths = function(m, n) {
-    
+const uniquePaths = (m, n) => {
+  const d = new Array(m).fill(undefined);
+  d.forEach(
+    (_, i, A) => A[i] = new Array(n).fill(1));
+
+  for(let i = 1; i < m; i++)
+    for(let j = 1; j < n; j++)
+      d[i][j] = d[i - 1][j] + d[i][j - 1];
+
+  return d[m - 1][n - 1];
 };
