36bac0fe6d6c5a5172bee5fc03b6ea0b45619aa9
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-04-24 18:09:09 -0500

    add solution

--- solutions/41/01.js
+++ solutions/41/01.js
@@ -2,6 +2,28 @@
  * @param {number[]} nums
  * @return {number}
  */
-var firstMissingPositive = function(nums) {
-    
+const firstMissingPositive = nums => {
+  const n = nums.length;
+
+  if(!nums.includes(1)) return 1;
+  if(n === 1)           return 2;
+
+  nums.forEach((e, i, A) => {
+    if(e <= 0 || e > n)
+      A[i] = 1;
+  });
+
+  nums.forEach((e, i, A) => {
+    const a = Math.abs(e);
+    if(a === n) A[0] = -Math.abs(A[0]);
+    else        A[a] = -Math.abs(A[a]);
+  });
+
+  for(let i = 1; i < n; i++)
+    if(nums[i] > 0)
+      return i;
+
+  if(nums[0] > 0) return n;
+
+  return n + 1;
 };
