7291f9747715b37ff7a479d05ba0a8f0e5251afa
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-05-03 13:19:10 -0500

    build Trie

--- solutions/212/01.js
+++ solutions/212/01.js
@@ -1,8 +1,32 @@
+
 /**
  * @param {character[][]} board
  * @param {string[]} words
  * @return {string[]}
  */
-var findWords = function(board, words) {
-    
+const findWords = (board, words) => {
 };
+
+const buildTrie = (paths, nullNode = '$') => {
+  const trie = new Map();
+
+  paths.forEach(path => {
+    let node = trie, child = undefined;
+
+    for(const x of path) {
+      if(undefined === (child = node.get(x))) {
+        child = new Map();
+        node.set(x, child);
+      }
+      node = child;
+    }
+
+    node.set(nullNode, path);
+  });
+
+  return trie;
+};
+
+const log = console.log;
+const words = ['oath', 'dig', 'dog', 'dogs'];
+const trie = buildTrie(words);
