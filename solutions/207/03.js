85756c1f965cb8099116c984daa5ed1662fd70a6
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-05-07 08:59:38 -0500

    add solution

--- solutions/207/03.js
+++ solutions/207/03.js
@@ -3,6 +3,65 @@
  * @param {number[][]} prerequisites
  * @return {boolean}
  */
-var canFinish = function(numCourses, prerequisites) {
-    
+const canFinish = (numCourses, prerequisites) => {
+  const graph = new Graph(numCourses);
+
+  prerequisites.forEach(([to, from]) =>
+    graph.addEdge(from, to));
+
+  return graph.topologicalSort().length;
 };
+
+class Graph {
+  constructor(nVertices) {
+    this.graph = new Map();
+    this.V = nVertices;
+  }
+
+  addEdge(u, v) {
+    let uEdges;
+    if(undefined ===
+                (uEdges = this.graph.get(u))) {
+      uEdges = [];
+      this.graph.set(u, uEdges);
+    }
+    uEdges.push(v);
+    return this;
+  }
+
+  topologicalSort() {
+    this.buildInDegrees();
+
+    const sourcesQueue = this.inDegs.reduce(
+      (a, inDeg, vertex) =>
+        inDeg ? a : a.concat(vertex),
+      []);
+
+    let visitCnt = 0;
+    const topOrder = [];
+
+    while(sourcesQueue.length) {
+      const u = sourcesQueue.shift();
+      topOrder.push(u);
+
+      let neighbors;
+      if(undefined ===
+                (neighbors = this.graph.get(u)))
+        neighbors = [];
+      for(const v of neighbors)
+        if(!--this.inDegs[v]) {
+          sourcesQueue.push(v);
+        }
+
+      visitCnt++;
+    }
+
+    return visitCnt !== this.V ? [] : topOrder;
+  }
+
+  buildInDegrees() {
+    this.inDegs = new Array(this.V).fill(0);
+    this.graph.forEach(v =>
+      v.forEach(e => this.inDegs[e]++));
+  }
+}
