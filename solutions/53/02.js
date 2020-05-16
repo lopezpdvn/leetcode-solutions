03c0788331ee33f13725e1198858eb25cb0bf373
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-05-15 22:47:30 -0500

    add solution

--- solutions/53/02.js
+++ solutions/53/02.js
@@ -2,6 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
-    
+const maxSubArray = nums => {
+  let currSum = -Infinity, maxSum = currSum;
+
+  nums.forEach(num => {
+    currSum = Math.max(num, currSum + num);
+    maxSum = Math.max(maxSum, currSum);
+  });
+
+  return maxSum;
 };
