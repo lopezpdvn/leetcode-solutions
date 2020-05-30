16233c79ed277d76b93bb4566f19ca88797e3451
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-05-30 02:52:01 -0500

    update solution

--- solutions/697/01.js
+++ solutions/697/01.js
@@ -2,24 +2,27 @@
  * @param {number[]} nums
  * @return {number}
  */
-const findShortestSubArray = nums => {
+const f = A => {
   const left = new Map(), right = new Map(),
     mset = new Map();
 
-  nums.forEach((e, i) => {
-    if(left.get(e) === undefined)
-      left.set(e, i);
+  A.forEach((e, i) => {
+    if(left.get(e) === undefined) left.set(e, i);
     right.set(e, i);
-    mset.set(e, (mset.get(e) || 0) + 1);
+    mset.set(e, (mset.get(e) || 0) + 1); // count
   });
 
-  let ans = nums.length;
   const degree = Math.max(...mset.values());
+  let ans = A.length;
 
   mset.forEach((count, e) => {
-    if(count === degree)
+    if(count === degree) {
       ans = Math.min(ans,
+
+        // degree of subarr containing all e's
         right.get(e) - left.get(e) + 1);
+
+    }
   });
 
   return ans;
