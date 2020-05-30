3b212e44bebd42104cf43f49365596bb10a04464
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-05-30 02:35:52 -0500

    add solution

--- solutions/697/01.js
+++ solutions/697/01.js
@@ -2,6 +2,25 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findShortestSubArray = function(nums) {
-    
+const findShortestSubArray = nums => {
+  const left = new Map(), right = new Map(),
+    mset = new Map();
+
+  nums.forEach((e, i) => {
+    if(left.get(e) === undefined)
+      left.set(e, i);
+    right.set(e, i);
+    mset.set(e, (mset.get(e) || 0) + 1);
+  });
+
+  let ans = nums.length;
+  const degree = Math.max(...mset.values());
+
+  mset.forEach((count, e) => {
+    if(count === degree)
+      ans = Math.min(ans,
+        right.get(e) - left.get(e) + 1);
+  });
+
+  return ans;
 };
