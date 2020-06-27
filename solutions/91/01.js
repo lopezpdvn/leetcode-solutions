32dffffd1d47acf3923c1d8c073d81035c33620b
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-06-26 23:22:19 -0500

    problem statement

--- solutions/91/01.js
+++ solutions/91/01.js
@@ -1,7 +1,11 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var numDecodings = function(s) {
-    
+/* A non-empty str containing letters A-Z is
+encoded to nums w/ A->1, B->2, ..., Z->26. Return
+# of ways to decode it.
+
+Ex1: '12'  -> 2 : 'AB' or 'L'
+Ex2: '226' -> 3 : 'BZ', 'VF' or 'BBF'
+
+numDecodings :: String -> Int                   */
+const numDecodings = s => {
+
 };
