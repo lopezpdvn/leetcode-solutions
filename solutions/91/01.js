0903502fd8e3ebe30bd5f65c6fdf0db87f868976
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-06-28 01:52:25 -0500

    add solution

--- solutions/91/01.js
+++ solutions/91/01.js
@@ -7,5 +7,22 @@ Ex2: '226' -> 3 : 'BZ', 'VF' or 'BBF'
 
 numDecodings :: String -> Int                   */
 const numDecodings = s => {
+  if(!s || !s.length) return 0;
+  return g(0, s, new Map());
+};
 
+const g = function f(i, s, memo) {
+  if(i === s.length)     return 1;
+  if(s[i] === '0')       return 0;
+  if(i === s.length - 1) return 1;
+
+  if(memo.has(i)) return memo.get(i);
+
+  let ans = f(i + 1, s, memo);
+  if(parseInt(s.substring(i, i + 2)) <= 26)
+    ans += f(i + 2, s, memo);
+
+  memo.set(i, ans);
+
+  return ans;
 };
