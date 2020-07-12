70e067de854cab39f7b2d3b8587fde419a126516
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-07-11 23:12:30 -0500

    accepted

--- solutions/271/02.js
+++ solutions/271/02.js
@@ -6,8 +6,14 @@
  * @param {string[]} strs
  * @return {string}
  */
-var encode = function(strs) {
-    
+const encode = strs => {
+  const sb = new Array() // string builder like
+  for(const str of strs) {
+    const x = uInt32ToStr(str.length);
+    sb.push(x);
+    sb.push(str);
+  }
+  return sb.join('');
 };
 
 /**
@@ -16,8 +22,38 @@ var encode = function(strs) {
  * @param {string} s
  * @return {string[]}
  */
-var decode = function(s) {
-    
+const decode = (s, nBytes = 4) => {
+  let i = 0;
+  const n = s.length, strs = new Array();
+
+  while(i < n) {
+    const currStrLen =
+      strToUInt32(s.substr(i, nBytes));
+    i += nBytes;
+    strs.push(s.substr(i, currStrLen));
+    i += currStrLen;
+  }
+
+  return strs;
+};
+
+const uInt32ToStr = (n, nBytes = 4) => {
+  const bytes = new Array(nBytes);
+  for(let i = nBytes - 1; i >= 0; i--) {
+    const maskByteSelector = i * 8;
+    const charCode = n >>> maskByteSelector;
+    bytes[nBytes - i - 1] = charCode;
+  }
+  return String.fromCharCode(...bytes);
+};
+
+const strToUInt32 = s => {
+  let uIntNum = 0;
+  for(let i = 0; i < s.length; i++) {
+    const charCode = s.charCodeAt(i);
+    uIntNum = (uInt32ToStr << 8) | charCode;
+  }
+  return uIntNum;
 };
 
 /**
@@ -25,14 +61,16 @@ var decode = function(s) {
  * decode(encode(strs));
  */
 
-const tests = [];
+const tests = [
+  ['Hello', 'World']
+];
 
 tests.forEach(test => {
-  const result = minWindow(...test[0]);
-  if(result === test[1])
-    log('OK');
-  else
-    log(`ErrOr: ${test}, ${result}`);
+  const result = decode(encode((test)));
+  const isEq = test.every(
+                      (e, i) => e === result[i]);
+  if(isEq) log('OK');
+  else     log(`Error: ${test}, ${result}`);
 });
 
 })();
