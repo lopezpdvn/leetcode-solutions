b4b2d0c978b255779067757ca520645b416e9bb8
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-07-17 23:08:49 -0500

    add solution

--- solutions/33/02.js
+++ solutions/33/02.js
@@ -2,7 +2,26 @@
 
 /* f :: [Double] -> Double -> Int */
 const search = (nums, target) => {
-    
+  let l = 0, h = nums.length - 1;
+
+  while(l <= h) {
+    const m = l + Math.trunc((h - l) / 2);
+    if(nums[m] === target)
+      return m;
+
+    if(nums[l] <= nums[m])
+      if(nums[l] <= target && target < nums[m])
+        h = m - 1;
+      else
+        l = m + 1;
+    else
+      if(nums[m] < target && target <= nums[h])
+        l = m + 1;
+      else
+        h = m - 1;
+  };
+
+  return -1;
 };
 
 })();
