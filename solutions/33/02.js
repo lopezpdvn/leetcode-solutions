e0bb6349653932faba1e4cc4e77e0adcc74d115a
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-07-17 22:58:35 -0500

    add template

---
+++ solutions/33/02.js
@@ -0,0 +1,12 @@
+'use strict'; const log = console.log; (async ()=>{
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    
+};
+
+})();
