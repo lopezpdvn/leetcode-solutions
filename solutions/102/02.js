314e4ed636959e525790e0bd5ccdcdb9695b1c07
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-07-18 23:39:54 -0500

    add solution

--- solutions/102/02.js
+++ solutions/102/02.js
@@ -1,18 +1,29 @@
 'use strict'; const log = console.log; (async ()=>{
 
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number[][]}
- */
-var levelOrder = function(root) {
-    
+const levelOrder = root => {
+  const levels = [];
+  if(!root) return levels;
+
+  let level = 0;
+  const queue = [root];
+
+  while(queue.length) {
+    levels.push([]);
+    const levelLen = queue.length;
+
+    for(let i = 0; i < levelLen; i++) {
+      const node = queue.shift();
+      levels[level].push(node.val);
+      if(node.left)
+        queue.push(node.left);
+      if(node.right)
+        queue.push(node.right);
+    }
+
+    level++;
+  }
+
+  return levels;
 };
 
 })();
