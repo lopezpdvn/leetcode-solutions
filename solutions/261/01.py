046f7180cc3d92f7200a637aafdd29b08086816f
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-07-20 13:35:44 -0500

    add solution

--- solutions/261/01.py
+++ solutions/261/01.py
@@ -1,13 +1,35 @@
 # Let G be a DG of n natural nums 0 to n - 1 & E
 # a [(v, u)] define edges v-u. Is G a tree? Exs:
 
-# 5 -> [(0,1), (0,2), (0,3), (1,4)]       -> True
+# 5 -> [(0,1), (0,2), (0,3), (1,4)]     -> True
 # 5 -> [(0,1), (1,2), (2,3), (1,3), (1,4)]-> False
 
 # f :: Int -> [(Int, Int)] -> Bool
 
+from collections import deque
+
 def f(n, edges):
-  return False
+  if len(edges) != n - 1: return False
+
+  adj_list = [[] for _ in range(n)]
+  for A, B in edges:
+    adj_list[A].append(B)
+    adj_list[B].append(A)
+
+  parent = {0: -1}
+  queue = deque([0])
+
+  while queue:
+    node = queue.popleft()
+    for neighbour in adj_list[node]:
+      if neighbour == parent[node]:
+        continue
+      if neighbour in parent:
+        return False
+      parent[neighbour] = node
+      queue.append(neighbour)
+
+  return len(parent) == n
 
 tests = [
   [5, [[0,1], [0,2], [0,3], [1,4]], True],
