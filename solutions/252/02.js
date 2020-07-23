b2aa543f2db4cdde304212fd150352e4947b9837
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-07-22 22:44:41 -0500

    set up and pseudocode

---
+++ solutions/252/02.js
@@ -0,0 +1,24 @@
+'use strict'; const log = console.log; (async ()=>{
+
+/* Let intervals be [(startTime, endTime)] of
+meetings, where startTime < endTime. Is it
+possible to attend all meetings?
+
+[(Double, Double)] -> Bool                     
+
+const f = intervals => {
+  
+  // sort intervals by startTime
+
+  // Compare endTime with starttime of adjacent
+  // meeting, return False early if overlap
+
+  // return True
+};                      */
+
+// Time complexity : O(n log(n))
+// Space complexity: O(1)
+
+// keywords: interval, overlap, intersection
+
+})();
