c2876fa354985c0509971dcedf3f6ef74973e70c
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-07-24 16:39:05 -0500

    add solution

---
+++ solutions/621/Solution2.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] map = new int[26];
+        for (char c: tasks)
+            map[c - 'A']++;
+        PriorityQueue < Integer > queue = new PriorityQueue < > (26, Collections.reverseOrder());
+        for (int f: map) {
+            if (f > 0)
+                queue.add(f);
+        }
+        int time = 0;
+        while (!queue.isEmpty()) {
+            int i = 0;
+            List < Integer > temp = new ArrayList < > ();
+            while (i <= n) {
+                if (!queue.isEmpty()) {
+                    if (queue.peek() > 1)
+                        temp.add(queue.poll() - 1);
+                    else
+                        queue.poll();
+                }
+                time++;
+                if (queue.isEmpty() && temp.size() == 0)
+                    break;
+                i++;
+            }
+            for (int l: temp)
+                queue.add(l);
+        }
+        return time;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        //int y = solution.leastInterval(new char[] {'A', 'A', 'A', 'B', 'B', 'B'}, 2);
+        int y = solution.leastInterval(new char[] {'A','A','A','A','A','A','B','C','D','E','F','G','Z'}, 2);
+       
+        System.out.println(y);
+    }
+}
