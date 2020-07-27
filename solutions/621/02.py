43e2c763ce981ff6f1a6570d32d22bcc18700ad2
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-07-27 00:09:50 -0500

    add solution

--- solutions/621/02.py
+++ solutions/621/02.py
@@ -3,10 +3,9 @@ from queue import PriorityQueue
 class Solution:
   def leastInterval(self, tasks, n):
     CHARSET_CARDINALITY = 26
-    CHARSET_ZERO_OFFSET = ord('a')
+    CHARSET_ZERO_OFFSET = ord('A')
 
     counter = [int()] * CHARSET_CARDINALITY
-
     for task in tasks:
       counter[ord(task) - CHARSET_ZERO_OFFSET] += 1
 
@@ -21,4 +20,27 @@ class Solution:
     time = 0
 
     while not max_heap.empty():
-      pass
+      j = 0
+      tmp = []
+
+      while j <= n:
+        if not max_heap.empty():
+          if -max_heap.queue[0][0] > 1:
+            tmp.append(-max_heap.get()[0] - 1)
+          else:
+            max_heap.get()
+
+        time += 1
+        if max_heap.empty() and not len(tmp):
+          break
+        j += 1
+
+      for e_tmp in tmp:
+        max_heap.put((-e_tmp, i))
+        i += 1
+
+    return time
+
+
+f = Solution()
+print(f.leastInterval(['A','A','A','B','B','B'], 2))
