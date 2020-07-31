4f078f91afcad18f39a8a9d3550ea3ae0d162165
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-07-31 18:19:52 -0500

    format

--- solutions/253/01.py
+++ solutions/253/01.py
@@ -1,32 +1,26 @@
-import heapq
+from queue import PriorityQueue
 
 class Solution:
-  def minMeetingRooms(self, intervals):
 
+  def minMeetingRooms(self, intervals):
     if not intervals: return 0
 
-    # The heap initialization
-    free_rooms = []
+    intervals.sort(key=lambda x: x[0]) # by start
+    desc_prioq = PriorityQueue()
+    prioq_index = 0
 
-    # Sort the meetings in increasing order of their start time.
-    intervals.sort(key= lambda x: x[0])
+    desc_prioq.put((intervals[0][1], prioq_index))
+    prioq_index += 1
 
-    # Add the first meeting. We have to give a new room to the first meeting.
-    heapq.heappush(free_rooms, intervals[0][1])
+    for start_time, end_time in intervals[1:]:
+      earliest_free_end = desc_prioq.queue[0][0]
+      if earliest_free_end <= start_time:
+        desc_prioq.get()
 
-    # For all the remaining meeting rooms
-    for i in intervals[1:]:
+      desc_prioq.put((end_time, prioq_index))
+      prioq_index += 1
 
-      # If the room due to free up the earliest is free, assign that room to this meeting.
-      if free_rooms[0] <= i[0]:
-        heapq.heappop(free_rooms)
-
-      # If a new room is to be assigned, then also we add to the heap,
-      # If an old room is allocated, then also we have to add to the heap with updated end time.
-      heapq.heappush(free_rooms, i[1])
-
-    # The size of the heap tells us the minimum rooms required for all the meetings.
-    return len(free_rooms)
+    return desc_prioq.qsize()
 
 f = Solution().minMeetingRooms
 assert f([(0, 30),(5, 10),(15, 20)]) == 2
