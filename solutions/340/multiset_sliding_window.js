694589abee5a5c85eb0bf7e5aaf69ec45c2f5ba2
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-08-15 17:50:51 -0500

    add solution

--- solutions/340/multiset_sliding_window.js
+++ solutions/340/multiset_sliding_window.js
@@ -1,9 +1,53 @@
 'use strict'; const log = console.log; (async ()=>{
 
-const f = (s, k) => {};
+const f = (s, k) => {
+  const sw = new SlidingWindowMultiSet();
+  let maxLen = 0;
 
-const testSetup = [[['eceba', 2], 3],
-  [['aa', 1], 2], [['loveleetcode', 4], 7]];
+  for(const c of s) {
+    sw.add(c);
+
+    while(sw.size > k && sw.length)
+      sw.delete(s[sw.L]);
+
+    maxLen = Math.max(maxLen, sw.length);
+  }
+
+  return maxLen;
+};
+
+class SlidingWindowMultiSet extends Map {
+  constructor() {
+    super();
+    this.L = 0;
+    this.R = -1;
+  }
+
+  add(e) {
+    this.R++;
+    let count = this.get(e) || 0;
+    this.set(e, ++count);
+    return count;
+  }
+
+  delete(e) {
+    this.L++;
+    let count = this.get(e) || 0;
+    if(count)
+      this.set(e, --count);
+    return count;
+  }
+
+  get length() {
+    return this.R - this.L + 1;
+  }
+}
+
+const testSetup = [
+  [['eceba', 2], 3],
+  [['aa', 1], 2],
+  [['loveleetcode', 4], 7]
+];
 
 for(const [args, answer] of testSetup) {
   if(f(...args) !== answer)
