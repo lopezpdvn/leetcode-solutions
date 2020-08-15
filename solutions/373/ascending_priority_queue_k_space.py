ecf13ae53df62a15d6209870a0476f8c473bd42f
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-08-14 21:49:59 -0500

    add solution

---
+++ solutions/373/ascending_priority_queue_k_space.py
@@ -0,0 +1,21 @@
+from queue import PriorityQueue
+from itertools import product
+
+def f(A, B, k):
+  if A is None or B is None or k < 0:
+    raise Error()
+
+  asc_prioq = PriorityQueue()
+  for a, b in product(A, B):
+    asc_prioq.put((-a - b, (a, b)))
+
+    if asc_prioq.qsize() > k:
+      asc_prioq.get()
+
+  return tuple(reversed(
+      [asc_prioq.get()[1] for i in range(k)
+                       if not asc_prioq.empty()]))
+
+assert f([1,7,11], [2,4,6], 3) == ((1,2), (1,4), (1,6))
+assert f([1,1,2], [1,2,3], 2) == ((1,1), (1,1))
+assert f([1,2], [3], 3) == ((1,3), (2,3))
