7220d7416b40085b009ab12dece9d679f4e49afc
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-08-17 13:20:04 -0500

    emulate enums and algebraic data types

--- solutions/545/pre_order_depth_first_traversal.js
+++ solutions/545/pre_order_depth_first_traversal.js
@@ -1,5 +1,26 @@
 'use strict'; const log = console.log; (async ()=>{
 
+const f = node => {
+  const LBndry = [], RBndry = [], leaves = [];
+  node.kind = BTNodeKind.Root;
+  PODFT(node, LBndry, RBndry, leaves);
+  return LBndry.concat(leaves, RBndry);
+};
+
+const PODFT = function f(
+                   node, LBndry, RBndry, leaves) {
+  if(!node) return;
+  node.tagChildren();
+  switch node.kind {
+    case BTNodeKind.RBndry:
+      break;
+  }
+};
+
+const BTNodeKind = {
+  Root: 0, LBndry: 1, RBndry: 2, Leaf: 3, Other: 4
+};
+
 // Binary Tree
 
 class BTNode {
@@ -12,6 +33,30 @@ class BTNode {
   *[Symbol.iterator]() {
     if(this.left)  yield this.left;
     if(this.right) yield this.right;
+  }
+
+  tagChildren()  {
+    this.tagLeftChild(); this.tagRightChild();
+  }
+
+  tagLeftChild() {
+    if(!this.left) return;
+    switch(this.kind) {
+      case BTNodeKind.Root:
+      case BTNodeKind.LBndry:
+        this.left.kind = BTNodeKind.LBndry;
+        break;
+      case BTNodeKind.RBndry:
+        if(!this.right) {
+          this.left.kind = BTNodeKind.RBndry;
+          break;
+        }
+      default:
+        this.left.kind =
+          !this.left.left && !this.left.right
+            ? BTNodeKind.Leaf
+            : BTNodeKind.Other;
+    }
   }
 }
 
