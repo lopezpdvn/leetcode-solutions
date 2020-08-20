ba7b03fff49f0d85ff299ef4a02e5a452ec79226
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-08-20 15:06:59 -0500

    add tests

--- solutions/295/03.py
+++ solutions/295/03.py
@@ -16,3 +16,13 @@ class MedianFinder:
       -self.lo.queue[0]                 if
       self.lo.qsize() > self.hi.qsize() else
       (-self.lo.queue[0] + self.hi.queue[0]) / 2.0)
+
+x = MedianFinder()
+x.addNum(1)
+x.addNum(2)
+assert x.findMedian() == 1.5
+x.addNum(3)
+assert x.findMedian() == 2
+x.addNum(99)
+x.addNum(100)
+assert x.findMedian() == 3
