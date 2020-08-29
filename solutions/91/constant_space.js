d3be1d26e0756b547e161339730f0bcab6e0c93a
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-08-28 19:25:49 -0500

    improve solution

--- solutions/91/constant_space.js
+++ solutions/91/constant_space.js
@@ -2,25 +2,24 @@ const f = s => {
   if(typeof s !== 'string')
     throw new Error();
 
-  let a = 1, b = 1, answer;
-  if(!s.length) return a;
+  let a = 1, b = 0;
+  if(!s) return a;
+
+  if(s[0] !== '0') b = 1;
   if(s.length === 1) return b;
 
+  let c = undefined;
   for(let i = 2; i <= s.length; i++) {
-    answer = 0;
+    c = 0;
+    const num = parseInt(s.substring(i - 2, i));
+    if(10 <= num && num <= 26); c += a;
 
-    if(s[i-1] !== '0')
-      answer += b;
-
-    const digit2 = parseInt(s.substring(i - 2, i));
-    if(10 <= digit2 && digit2 <= 26)
-      answer += a;
-
+    if(s[i-1] !== '0') c += b;
     a = b;
-    b = answer;
+    b = c;
   }
 
-  return answer;
+  return c;
 };
 
 const testSetup = [
