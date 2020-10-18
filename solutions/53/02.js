609719f95a79c52e3923e20d6029fd7688789a27
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-10-18 04:54:54 -0500

    add tests

--- solutions/53/02.js
+++ solutions/53/02.js
@@ -12,3 +12,13 @@ const maxSubArray = nums => {
 
   return maxSum;
 };
+
+const testSetup = [
+  [[-2, 1, -3, 4, -1, 2, 1, -5, 4], 6],
+  [[2], 2],
+  [[-5, -5, 2], 2],
+  [[2, -8, 3, -2, 4, -10], 5]
+], eq = require('assert').strictEqual;
+
+for(const [args, ans] of testSetup)
+  eq(maxSubArray(args), ans);
