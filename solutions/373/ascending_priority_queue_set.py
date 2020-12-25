bb02e9c075cf26480dd8154cc6d5125ad161c65a
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-12-24 20:41:34 -0600

    works

---
+++ solutions/373/ascending_priority_queue_set.py
@@ -0,0 +1,33 @@
+from queue import PriorityQueue
+from itertools import product
+
+def f(A, B, k):
+  if A is None or B is None or k < 0:
+    raise Exception()
+
+  asc_prioq = PriorityQueue()
+  asc_prioq.put((A[0] + B[0], 0, 0))
+  indices = {(0, 0)}
+  ans = []
+  nMax = min(k, len(A) * len(B))
+
+  for i in range(nMax):
+      a, b = asc_prioq.get()[1:]
+      ans.append((A[a], B[b]))
+
+      if a+1 < len(A) and (a+1, b) not in indices:
+          asc_prioq.put((A[a+1] + B[b], a+1, b))
+          indices.add((a+1, b))
+
+      if b+1 < len(B) and (a, b+1) not in indices:
+          asc_prioq.put((A[a] + B[b+1], a, b+1))
+          indices.add((a, b+1))
+
+  return tuple(ans)
+
+assert f([1,7,11], [2,4,6], 3) == (
+                              (1,2), (1,4), (1,6))
+assert f([1,1,2], [1,2,3], 2) == ((1,1), (1,1))
+assert f([1,2], [3], 3) == ((1,3), (2,3))
+assert f([1,3,11], [2,4,8], 4) == (
+                           (1,2),(1,4),(3,2),(3,4))
