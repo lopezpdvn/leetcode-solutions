05fba211ae03dc411bea553a3fed7c69df1afe83
Pedro Ivan Lopez <m@pedroivanlopez.com>
2021-01-07 19:02:42 -0600

    add solution

---
+++ solutions/435/sort_greedy.py
@@ -0,0 +1,22 @@
+def f(A):
+    if not A: return 0
+
+    A.sort()
+    prev = A[0]
+    count = 0
+
+    for curr in A[1:]:
+        if prev[1] <= curr[0]:
+            prev = curr
+            continue
+
+        count += 1
+
+        if prev[1] >= curr[1]:
+            prev = curr
+
+    return count
+
+assert f([[1,2], [2,3], [3,4], [1,3]]) == 1
+assert f([[1,2], [1,2], [1,2]]) == 2
+assert f([[1,2], [2,3]]) == 0
