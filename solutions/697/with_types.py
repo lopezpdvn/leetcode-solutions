2996519319b7ac504f75d5e2847da1d427af917e
Pedro Ivan Lopez <m@pedroivanlopez.com>
2021-01-21 17:36:18 -0600

    add example and override __len__

--- solutions/697/with_types.py
+++ solutions/697/with_types.py
@@ -5,8 +5,7 @@ class SlidingWindow:
     def __init__(self, L = 0, R = -1):
         self.L = L
         self.R = R
-    @property
-    def length(self):
+    def __len__(self):
         return self.R - self.L + 1
 
 def f(A):
@@ -25,9 +24,10 @@ def f(A):
     for e, sw in sws.items():
         if mset[e] != A_deg:
             continue
-        shortst_len = min(shortst_len, sw.length)
+        shortst_len = min(shortst_len, len(sw))
 
     return shortst_len
 
+assert f([9]) == 1
 assert f([1,2,2,3,1]) == 2
 assert f([1,2,2,3,1,4,2]) == 6
