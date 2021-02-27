6011760991416fdc2a31588a8ccec1709ca4c8bf
Pedro Ivan Lopez <m@pedroivanlopez.com>
2021-02-26 18:55:08 -0600

    fix bug

--- solutions/91/constant_space.py
+++ solutions/91/constant_space.py
@@ -1,21 +1,22 @@
 def f(s):
     if s is None: raise Exception()
-    if not s: return 1
 
     n = len(s)
-    if n == 1 and s != '0': return 1
-
     a = 1
-    b = 0 if s == '0' else 1
+    if not n: return a
 
-    c = 0
-    for i in range(2, n+1):
+    b = 0 if s[0] == '0' else 1
+    if n == 1: return b
+
+    for i in range(2, n + 1):
         c = 0
 
-        if s[i-1] != '0':   c += a
+        x = int(s[i - 2:i])
+        if 10 <= x <= 26:
+            c += a
 
-        num = int(s[i-2:i])
-        if 10 <= num <= 26: c += b
+        if s[i - 1] != '0':
+            c += b
 
         a, b = b, c
 
@@ -23,7 +24,7 @@ def f(s):
 
 assert f('') == 1
 assert f('12') == 2
-assert f('09') == 1
+assert f('09') == 0
 assert f('226') == 3
 assert f('2') == 1
 assert f('111') == 3
