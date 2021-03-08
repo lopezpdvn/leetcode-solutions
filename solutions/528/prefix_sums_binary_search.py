eb47863174ad9d8fddff29f7565fa06461bb78bb
Pedro Ivan Lopez <m@pedroivanlopez.com>
2021-03-08 01:07:54 -0600

    improve scan / prefix sum

--- solutions/528/prefix_sums_binary_search.py
+++ solutions/528/prefix_sums_binary_search.py
@@ -1,11 +1,9 @@
 class Solution:
   def __init__(self, w: List[int]):
-    self.prefix_sums = []
-    prefix_sum = 0
-    for weight in w:
-      prefix_sum += weight
-      self.prefix_sums.append(prefix_sum)
-    self.total_sum = prefix_sum
+    from itertools import accumulate
+    from operator import add
+    self.prefix_sums = (*accumulate(w, add),)
+    self.total_sum = self.prefix_sums[-1]
 
   def pickIndex(self) -> int:
     target = self.total_sum * random.random()
