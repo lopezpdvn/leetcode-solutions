44bdc9c3ab7b6b3f26f9a693a33f9d85e79a084a
Pedro Ivan Lopez <m@pedroivanlopez.com>
2021-04-14 23:55:12 -0500

    format

--- solutions/124/iodft_recursive_closure.py
+++ solutions/124/iodft_recursive_closure.py
@@ -1,28 +1,30 @@
 class Solution:
-    def maxPathSum(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        def max_gain(node):
-            nonlocal max_sum
-            if not node:
-                return 0
+  def maxPathSum(self, root):
+    """
+    :type root: TreeNode
+    :rtype: int
+    """
+    def max_gain(node):
+      nonlocal max_sum
+      if not node:
+        return 0
 
-            # max sum on the left and right sub-trees of node
-            left_gain = max(max_gain(node.left), 0)
-            right_gain = max(max_gain(node.right), 0)
-            
-            # the price to start a new path where `node` is a highest node
-            price_newpath = node.val + left_gain + right_gain
-            
-            # update max_sum if it's better to start a new path
-            max_sum = max(max_sum, price_newpath)
-        
-            # for recursion :
-            # return the max gain if continue the same path
-            return node.val + max(left_gain, right_gain)
+      # max sum on the L & R sub-trees of node
+      L_gain = max(max_gain(node.left), 0)
+      R_gain = max(max_gain(node.right), 0)
+      
+      # price 2 start a new path where node is a
+      # highest node
+      price_newpath = node.val + L_gain + R_gain
+      
+      # update max_sum if it's better 2 start
+      # a new path
+      max_sum = max(max_sum, price_newpath)
+    
+      # for recursion:
+      # return max gain if continue same path
+      return node.val + max(L_gain, R_gain)
    
-        max_sum = float('-inf')
-        max_gain(root)
-        return max_sum
+    max_sum = float('-inf')
+    max_gain(root)
+    return max_sum
